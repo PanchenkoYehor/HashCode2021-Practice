a8cf1d1d7cba8ebd78b62a73c4578d323494cd6c
Yehor Panchenko <51265778+eR6ik@users.noreply.github.com>
2021-01-19 22:46:49 +0200

    Main.cpp has been rewritten

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <cmath>
+#include <ctime>
+#include <random>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    freopen("a.in", "r", stdin);
+    int n, t1, t2, t3;
+    cin>>n>>t1>>t2>>t3;
+    vector < vector < string > > p(n);
+    map<string,int> ingr;
+    //vector < int > unic(n);
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin>>k;
+        p[i].resize(k);
+        for (int j = 0; j < k; j++) {
+            cin>>p[i][j];
+            ingr[p[i][j]]++;
+        }
+    }
+    cout<<ingr.size()<<"\n";
 }
